5233923cf13483adc527b28dcd579695e593617e
Julien Whitfield <julien.whitfield@etu.unice.fr>
2022-11-26 17:53:07 +0100

    Added smartcard to allow APDU communication
    Signed-off-by: Julien Whitfield <julien.whitfield@etu.unice.fr>

--- project/terminal.py
+++ project/terminal.py
@@ -1,21 +1,30 @@
 import random
+from smartcard.System import readers
+from smartcard.util import toHexString
 
-
-def verify_PIN(pin):
+def verify_PIN(pin, card):
+    SELECT = [0x01, 0x00, 0x00, 0x00, 0x02]
+    DATA = [0x00, 0x01]
+    response, sw1, sw2 = card.transmit( SELECT + DATA )
+    print(response)
+    print(sw1)
+    print(sw2)
     return True if random.randint(0, 10) == 0 else False
 
 
-def encrypt_string(string):
+def encrypt_string(string, card):
     return "hi"
 
-
 def main():
+
+    card = readers()[0].createConnection()
+    card.connect()
 
     right_PIN = False
     while not right_PIN:
         print("Please enter the 4-digit PIN code: ", end="")
         user_input = input()
-        right_PIN = verify_PIN(user_input)
+        right_PIN = verify_PIN(user_input, card)
         print("PIN code correct." if right_PIN else "Incorrect PIN code.")
 
     while True:
